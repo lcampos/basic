a6eea93a7ffb80a582022a5e72da131e1bb11aaa
Luis Campos Guajardo <luis@zuplo.com>
2024-10-03 10:50:16 -0600

    Set env var in gh action with js

---
+++ scripts/set-env-vars.js
@@ -0,0 +1,8 @@
+// scripts/set-env.js
+const fs = require('fs');
+
+// Value to set for the environment variable
+const myValue = 'Hello from JS script';
+
+// Write the variable to the GITHUB_ENV file
+fs.appendFileSync(process.env.GITHUB_ENV, `MY_VARIABLE=${myValue}\n`);
